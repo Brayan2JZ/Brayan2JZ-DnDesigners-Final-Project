fe03de50f65a4d56e2c842a313638cc1b43e252b
Brayan2JZ <b.sredzinski92@gmail.com>
2024-10-31 00:07:44 +0000

    Misc Page added/needs to be worked on

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,6 +12,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Misc } from "./pages/misc";
 
 //create your first component
 const Layout = () => {
@@ -30,6 +31,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Misc />} path="/misc" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
---
+++ src/front/js/pages/misc.js
@@ -0,0 +1,8 @@
+import React, { useRef, useState } from 'react';
+import { MyComponent } from './makeImage';
+
+export const Misc = () => (
+        <div>
+            <h1>This is the Misc Page</h1>
+        </div>
+    );
