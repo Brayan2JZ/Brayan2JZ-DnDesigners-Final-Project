5d84dabb446486693e2101dc2b60f173405ed247
Brayan2JZ <b.sredzinski92@gmail.com>
2024-10-31 00:42:40 +0000

    git

--- src/front/js/pages/misc.js
+++ src/front/js/pages/misc.js
@@ -2,7 +2,12 @@ import React, { useRef, useState } from 'react';
 import { MyComponent } from './makeImage';
 
 export const Misc = () => (
+    
         <div>
             <h1>This is the Misc Page</h1>
+
+            <div className="container">
+                
+            </div>
         </div>
     );
